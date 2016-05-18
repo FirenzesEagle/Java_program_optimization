d3c04fab7a02618361fbc40a8cae08030d737978
FirenzesEagle <liumingbo2008@gmail.com>
2016-05-18 21:05:27 +0800

    complete ValueObject_Pattern code

---
+++ src/main/java/cn/edu/bupt/ValueObject_Pattern/OrderManager.java
@@ -0,0 +1,45 @@
+package cn.edu.bupt.ValueObject_Pattern;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+/**
+ * Created by FirenzesEagle on 2016/5/18 0018.
+ * Email:liumingbo2008@gmail.com
+ */
+public class OrderManager extends UnicastRemoteObject implements IOrderManager {
+
+    protected OrderManager() throws RemoteException {
+        super();
+    }
+
+    /**
+     * @param id
+     * @return 订单信息
+     * @throws RemoteException
+     */
+    @Override
+    public Order getOrder(int id) throws RemoteException {
+        Order order = new Order();
+        order.setClientName("billy");
+        order.setNumber(20);
+        order.setProductName("desk");
+        return order;
+    }
+
+    @Override
+    public String getClientName(int id) throws RemoteException {
+        return "billy";
+    }
+
+    @Override
+    public String getProductName(int id) throws RemoteException {
+        return "desk";
+    }
+
+    @Override
+    public int getNumber(int id) throws RemoteException {
+        return 20;
+    }
+
+}
